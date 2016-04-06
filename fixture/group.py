a322dd26750165148cb81fe474a793c1b8ea011d
vp96288 <vyacheslav1.pak@gmail.com>
2016-04-06 00:12:32 -0700

    Задание №10:
    оптимизировать переходы между страницами

--- fixture/group.py
+++ fixture/group.py
@@ -8,15 +8,14 @@ class GroupHelper:
 
     def open_groups_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
+        if not wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0:
+            wd.find_element_by_link_text("groups").click()
 
     def create(self, group):
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
-        # init group creation
         wd.find_element_by_name("new").click()
         self.fill_group_form(group)
-        # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
