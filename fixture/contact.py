21bf9e5b5f00dfc2fcfb9c3c0a087a90d36ff398
vp96288 <vyacheslav1.pak@gmail.com>
2016-04-05 23:44:04 -0700

    Задание №9:
    Обеспечить выполнение предусловий в тестах для удаления и модификации
    для групп и контактов

--- fixture/contact.py
+++ fixture/contact.py
@@ -65,3 +65,8 @@ class ContactHelper:
     def select_first_contact(self):
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
+
+
+    def count(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_name("selected[]"))
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -2,7 +2,11 @@
 from model.contact import Contact
 
 def test_modify_firstname_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname = "test"))
     app.contact.modify_first_contact(Contact(firstname="New firstname"))
 
 def test_modify_lastname_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname = "test"))
     app.contact.modify_first_contact(Contact(lastname="New lastname"))
